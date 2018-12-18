b1c9d7caf435bf7727b46346ac9166c660d93c05
shadow.reyson <shadow.reyson@gmail.com>
2018-12-18 15:04:25 +0100

    rapport

--- TP6/client.py
+++ TP6/client.py
@@ -10,7 +10,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("192.168.1.10", UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(320 * 180 * 8)
+    data, addr = sock.recvfrom(320 * 180)
     image = np.frombuffer(data, dtype='int8')
     image = np.uint8(image)
     image = np.reshape(image, (180, 320))
